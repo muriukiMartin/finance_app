aacc74071fd0c34fc7bc78945005f141f2659c64
Martin <maritn.muriuki@strathmore.edu>
2022-02-19 15:19:57 +0300

    Create bar chart

---
+++ lib/widgets/chart_bar.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class ChartBar extends StatelessWidget {
+  final String label;
+  final double spentAmount;
+  final double spentPercentage;
+
+  ChartBar(this.label, this.spentAmount, this.spentPercentage);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Text('Shs${spentAmount.toStringAsFixed(0)}'),
+        SizedBox(
+          height: 4,
+        ),
+        Container(
+          height: 60,
+          width: 10,
+          child: Stack(children: <Widget>[
+            Container(
+              decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey, width: 1.0),
+                  color: Color.fromRGBO(220, 220, 220, 1),
+                  borderRadius: BorderRadius.circular(10)),
+            ),
+            FractionallySizedBox(
+              heightFactor: spentPercentage,
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColor,
+                    borderRadius: BorderRadius.circular(10)),
+              ),
+            )
+          ]),
+        ),
+        SizedBox(
+          height: 4,
+        ),
+        Text(label),
+      ],
+    );
+  }
+}
