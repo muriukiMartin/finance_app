e6c5b5e5f84c4d1fd93dcf4eafeb3b32816d83f2
Martin <maritn.muriuki@strathmore.edu>
2022-02-19 15:20:08 +0300

    Populate bar chart

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import '../models/transaction.dart';
+import './chart_bar.dart';
 
 class Chart extends StatelessWidget {
   final List<Transaction> recentTransactions;
@@ -21,19 +22,30 @@ class Chart extends StatelessWidget {
           totalSum += recentTransactions[i].amount;
         }
       }
-      
-      return {'day': DateFormat.E().format(weekDay), 'amount': 9.99};
+
+      return {
+        'day': DateFormat.E().format(weekDay).substring(0, 3),
+        'amount': totalSum
+      };
+    });
+  }
+
+  double get totalSpending {
+    return groupedTransactionsValues.fold(0.0, (sum, item) {
+      return sum + item['amount'];
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    
     return Card(
       elevation: 6,
       margin: EdgeInsets.all(20),
       child: Row(
-        children: <Widget>[],
+        children: groupedTransactionsValues.map((data) {
+          return ChartBar(
+              data['day'], data['amount'], (data['amount']  as double) / totalSpending);
+        }).toList(),
       ),
     );
   }
